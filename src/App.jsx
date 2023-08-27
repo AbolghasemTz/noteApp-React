eb3a2ffbc71983d1dfba75d143c61dc378dee955
Abolghasem Taziki <113670236+AbolghasemTz@users.noreply.github.com>
2023-08-27 14:03:41 +0330

    delete note

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import "./App.css";
 import AddNewNote from "./components/AddNewNote";
 import NoteList from "./components/NoteList";
 function App() {
-  const [notes,setNotes] = useState([])
+  const [notes, setNotes] = useState([]);
 
   const handelNote = (newNote) => {
-    setNotes((prevState) => [...prevState,newNote])
-  }
-  return <div className="container">
-   <div className="note-header">note header</div>
-   <div className="note-app">
-   <AddNewNote addNote={handelNote}/>
-   <div className='note-container'>
-    <NoteList notes={notes}/>
+    setNotes((prevState) => [...prevState, newNote]);
+  };
+
+  const handleDeleteNote = (id) => {
+    // const filteredNotes = notes.filter((n) => n.id !== id);
+    // setNotes(filteredNotes)
+    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+  };
+  return (
+    <div className="container">
+      <div className="note-header">note header</div>
+      <div className="note-app">
+        <AddNewNote addNote={handelNote} />
+        <div className="note-container">
+          <NoteList notes={notes} onDeleteNote={handleDeleteNote} />
+        </div>
+      </div>
     </div>
-   </div>
-  </div>;
+  );
 }
 
 export default App;
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import NoteItem from './NoteItem';
 
-function NoteList({notes}) {
+function NoteList({notes,onDeleteNote}) {
     console.log(notes);
   return (
     <div className='note-list'>
       {notes.map((n) => {
-        return <NoteItem key={notes.id} note={n}/>
+        return <NoteItem key={n.id} note={n} onDeleteNote={onDeleteNote}/>
       })}
     </div>
   )
