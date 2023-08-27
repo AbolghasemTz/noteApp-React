072a1044cc8e6f78f7b409af2934f3c5b7dd80b2
Abolghasem Taziki <113670236+AbolghasemTz@users.noreply.github.com>
2023-08-27 09:59:40 +0330

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 function App() {
-  return <div className="">
-   
+  return <div className="container">
+   <div className="note-header">note header</div>
+   <div className="note-app">
+    <div className="add-new-note">add new noote form</div>
+    <div className="note-container">motes</div>
+   </div>
   </div>;
 }
 
